a399e57ac62d4cb878c926481375bd4a66848be0
William Perkola <perkola.william@gmail.com>
2016-05-03 11:49:21 +0200

    Add sass to default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('sass:watch', function() {
+gulp.task('watch', function() {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass', 'watch']);
