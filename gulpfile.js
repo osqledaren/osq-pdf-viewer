cb868348b78e778bc10734e098f415ae6470baa1
Arvid Sätterkvist <arvidsat@gmail.com>
2016-05-03 12:04:05 +0200

    started working on pdf-stacks

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./css'));
 });
 
--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-pdfGrid = document.getElementById('pdf-grid');
+pdfGrid = document.getElementById('pdf-stack');
 
-var meta = [{
-    'pdf': 'http://osqledaren.se/wp-content/uploads/2016/05/Osq1603s5-1.pdf',
-    'frontpage': 'frontpage.jpg',
-    'editorInChief': 'Tania Christensen',
-    'year': 2015
-}];
+var meta = [
+    {
+        'pdf': 'http://osqledaren.se/wp-content/uploads/2016/05/Osq1603s5-1.pdf',
+        'frontpage': 'frontpage.jpg',
+        'editorInChief': 'Tania Christensen',
+        'year': 2015
+    },
+    {
+        'pdf': 'http://osqledaren.se/wp-content/uploads/2016/05/Osq1603s5-1.pdf',
+        'frontpage': 'frontpage.jpg',
+        'editorInChief': 'Tania Christensen',
+        'year': 2015
+    },
+    {
+        'pdf': 'http://osqledaren.se/wp-content/uploads/2016/05/Osq1603s5-1.pdf',
+        'frontpage': 'frontpage.jpg',
+        'editorInChief': 'Tania Christensen',
+        'year': 2015
+    },
+    {
+        'pdf': 'http://osqledaren.se/wp-content/uploads/2016/05/Osq1603s5-1.pdf',
+        'frontpage': 'frontpage.jpg',
+        'editorInChief': 'Tania Christensen',
+        'year': 2015
+    }
+];
 
 for (var i in meta) {
     pdfGrid.innerHTML = pdfGrid.innerHTML + '<img src=' + meta[i].frontpage + ' class="pdf"/>';
